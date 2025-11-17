1e8b027610a9b2de7c08f324248c40d4f4c18cd0
AqilYogaPramono <aqilyogapra1@gmail.com>
2025-11-18 03:14:51 +0700

    feat: CRUD routes and views tag

--- models/Tag.js
+++ models/Tag.js
@@ -1,7 +1,68 @@
 const connection = require('../configs/database')
 
 class Tag {
+    static async getAll() {
+        try {
+            const [rows] = await connection.query(`SELECT * FROM tag ORDER BY id ASC`)
+            return rows
+        } catch (err) {
+            throw err
+        }
+    }
 
+    static async store(data) {
+        try {
+            const [result] = await connection.query(`INSERT INTO tag SET ?`, [data])
+            return result
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async update(data, id) {
+        try {
+            const [result] = await connection.query(`UPDATE tag SET ? WHERE id = ?`, [data, id])
+            return result
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async getById(id) {
+        try {
+            const [rows] = await connection.query(`SELECT * FROM tag WHERE id = ?`, [id])
+            return rows[0]
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async delete(id) {
+        try {
+            const [result] = await connection.query(`DELETE FROM tag WHERE id = ?`, [id])
+            return result
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async checkTagCreate(data) {
+        try {
+            const [rows] = await connection.query(`SELECT nama_tag FROM tag WHERE nama_tag = ?`, [data.nama_tag])
+            return rows.length > 0
+        } catch (err) {
+            throw err
+        }
+    }
+
+    static async checkTagUpdate(data, id) {
+        try {
+            const [rows] = await connection.query(`SELECT nama_tag FROM tag WHERE nama_tag = ? AND id != ?`, [data.nama_tag, id])
+            return rows.length > 0
+        } catch (err) {
+            throw err
+        }
+    }
 }
 
 module.exports = Tag
--- routes/pustakawan/tag.js
+++ routes/pustakawan/tag.js
@@ -1,7 +1,124 @@
 const express = require('express')
 
-
+const Tag = require('../../models/Tag')
+const Pegawai = require('../../models/Pegawai')
+const {authPustakawan} = require('../../middlewares/auth')
 
 const router = express.Router()
+
+router.get('/', authPustakawan, async (req, res) => {
+    try {
+        const pegawai = await Pegawai.getNama(req.session.pegawaiId)
+        const data = await Tag.getAll()
+
+        res.render('pustakawan/tag/index', {data, pegawai})
+    } catch(err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        return res.redirect('/pustakawan/tag')
+    }
+})
+
+router.get('/buat', authPustakawan, async (req, res) => {
+    try {
+        const pegawai = await Pegawai.getNama(req.session.pegawaiId)
+
+        res.render('pustakawan/tag/buat', { 
+            pegawai,
+            data: req.flash('data')[0]
+        })
+    } catch(err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        return res.redirect('/pustakawan/tag')
+    }
+})
+
+router.post('/create', authPustakawan, async (req, res) => {
+    try {
+        const {nama_tag} = req.body
+
+        const data = {nama_tag}
+
+        if (!data.nama_tag) {
+            req.flash("error", "Nama tag tidak boleh kosong")
+            req.flash('data', req.body)
+            return res.redirect('/pustakawan/tag/buat')
+        }
+
+        if (await Tag.checkTagCreate(data)) {
+            req.flash("error", "Tag sudah dibuat")
+            req.flash('data', req.body)
+            return res.redirect('/pustakawan/tag/buat')
+        }
+
+        await Tag.store(data)
+        req.flash('success', 'Data Berhasil Ditambahkan')
+        res.redirect('/pustakawan/tag')
+    } catch(err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        return res.redirect('/pustakawan/tag')
+    }
+})
+
+router.get('/edit/:id', authPustakawan, async(req, res) => {
+    try {
+        const {id} = req.params
+
+        const pegawai = await Pegawai.getNama(req.session.pegawaiId)
+        const data = await Tag.getById(id)
+
+        res.render('pustakawan/tag/edit', {data, pegawai})
+    } catch(err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        return res.redirect('/pustakawan/tag')
+    }
+})
+
+router.post('/update/:id', authPustakawan, async (req, res) => {
+    try {
+        const {id} = req.params
+        const {nama_tag} = req.body
+
+        const data = {nama_tag}
+        
+        if (!nama_tag) {
+            req.flash("error", "Nama tag tidak boleh kosong")
+            req.flash('data', req.body)
+            return res.redirect(`/pustakawan/tag/edit/${id}`)
+        }
+
+        if (await Tag.checkTagUpdate(data, id)) {
+            req.flash("error", "Tag sudah dibuat")
+            req.flash('data', req.body)
+            return res.redirect(`/pustakawan/tag/edit/${id}`)
+        }
+
+        await Tag.update(data, id)
+        req.flash('success', 'Data Berhasil Diedit')
+        res.redirect('/pustakawan/tag')
+    } catch (err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        req.flash('data', req.body)
+        return res.redirect('/pustakawan/tag')
+    }
+})
+
+router.post('/delete/:id', authPustakawan, async (req, res) => {
+    try {
+        const {id} = req.params
+
+        await Tag.delete(id)
+        req.flash('success', 'Data Berhasil Dihapus')
+        res.redirect('/pustakawan/tag')
+    } catch(err) {
+        console.error(err)
+        req.flash('error', "Internal Server Error")
+        return res.redirect('/pustakawan/tag')
+    }
+})
 
 module.exports = router
